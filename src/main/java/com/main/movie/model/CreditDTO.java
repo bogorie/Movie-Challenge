1a4c045be0b281260d152aaa502be049a50ae946
Rie <rkaneko@javeriana.edu.co>
2020-05-16 02:29:29 -0500

    Add cast endpoint, fixed nullpointer exception

---
+++ src/main/java/com/main/movie/model/CreditDTO.java
@@ -0,0 +1,15 @@
+package com.main.movie.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class CreditDTO {
+    private Integer id;
+    private List<CastDTO> cast;
+}
--- src/main/java/com/main/movie/model/Response.java
+++ src/main/java/com/main/movie/model/Response.java
@@ -4,16 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigInteger;
-
 @AllArgsConstructor
 @Getter
 @Setter
 public class Response {
-    private Integer movieId;
+    private int movieId;
     private String title;
     private String genres;
+    private float rating;
     private String poster_path;
     private String release_date;
-    private BigInteger budget;
+    private float budget;
+    private String overview;
 }
