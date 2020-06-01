4477c1aa16091fa97d9226f2fdae940e4872cba7
Rie <rkaneko@javeriana.edu.co>
2020-05-31 21:26:57 -0500

    Added CrossOrigin

--- src/main/java/com/main/movie/controller/MovieController.java
+++ src/main/java/com/main/movie/controller/MovieController.java
@@ -20,27 +20,32 @@ public class MovieController {
         movieService = movieServiceImp;
     }
 
+    @CrossOrigin
     @GetMapping("/movie/{movie_id}")
     public Mono<MovieDTO> getMovie(@PathVariable Integer movie_id) {
         return movieService.getMovie(movie_id);
 
     }
 
+    @CrossOrigin
     @GetMapping("/movie/detail/{movie_id}")
     public Mono<MovieDetail> getMovieDetails(@PathVariable Integer movie_id) {
         return movieService.getApiDetails(movie_id);
     }
 
+    @CrossOrigin
     @GetMapping("/movie/casts/{movie_id}")
     public Mono<CreditDTO> getApiCast(@PathVariable Integer movie_id) {
         return movieService.getApiCast(movie_id);
     }
 
+    @CrossOrigin
     @GetMapping("/movie/rating/{movie_id}")
     public Mono<MovieRatingHistoryResponse> getMovieRatingHistory(@PathVariable Integer movie_id){
         return movieService.getMovieRatingHistory(movie_id);
     }
 
+    @CrossOrigin
     @GetMapping("/moviesDB")
     public Flux<MovieDBResponse> getMovies(@RequestParam(required = false) Optional<String> sort,
                                     @RequestParam(required = false) Optional<String> genres,
@@ -50,6 +55,7 @@ public class MovieController {
         return movieService.getMoviesFromDB(sort, genres, limit, page, title);
     }
 
+    @CrossOrigin
     @GetMapping("/movies")
     public Flux<MovieResponse> getMovieData(@RequestParam(required = false) Optional<String> sort,
                                             @RequestParam(required = false) Optional<String> genres,
@@ -59,6 +65,7 @@ public class MovieController {
         return movieService.getMoviesData(sort, genres, limit, page, title);
     }
 
+    @CrossOrigin
     @GetMapping("/movies/genres")
     public Mono<GenreResponse> getMoviesGenres(){
         return movieService.getMoviesGenres();
