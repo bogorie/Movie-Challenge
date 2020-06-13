d8ea2d89f90fb5e69128a69f6748692d1e5517d3
Raul Ramirez <raaramirezpe@unal.edu.co>
2020-06-13 13:38:24 -0500

    revert hotfix changes

--- src/main/java/com/main/movie/repository/LinkRepository.java
+++ src/main/java/com/main/movie/repository/LinkRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface LinkRepository extends CrudRepository<LinkDTO, Integer> {
-    @Query( value = "select l.tmdb_id from link l where l.movie_id=?1 LIMIT 1 ",
+    @Query( value = "select l.tmdb_id from link l where l.movie_id=?1",
             nativeQuery = true)
     Optional<Integer> findTmdbId(Integer movie_id);
 }
