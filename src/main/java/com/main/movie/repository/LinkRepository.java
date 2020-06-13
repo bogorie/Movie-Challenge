6f00689b7e3faceb916e7e4b9fe9ddf8ba73b3d2
Raul Ramirez <raaramirezpe@unal.edu.co>
2020-06-13 12:55:18 -0500

    Fix while dulpicate data is removed from DB

--- src/main/java/com/main/movie/repository/LinkRepository.java
+++ src/main/java/com/main/movie/repository/LinkRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface LinkRepository extends CrudRepository<LinkDTO, Integer> {
-    @Query( value = "select l.tmdb_id from link l where l.movie_id=?1",
+    @Query( value = "select l.tmdb_id from link l where l.movie_id=?1 group by imdb_id, tmdb_id,  movie_id ",
             nativeQuery = true)
     Optional<Integer> findTmdbId(Integer movie_id);
 }
